f801e36b82b9922df4c506886ad4dd8a04b72aa1
Divya Yash <divyayashsaxena2000@gmail.com>
2022-10-23 07:46:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const con = require("./config");
 const app = express();
 
 
-app.get("/", (req, resp) => {
+app.get("/", (req, res) => {
+ 
+res.send("BPH Sports API is up and running") 
+
+});
+
+app.get("/websites", (req, res) => {
   con.query("select * from collection where 1", (err, result) => {
-    if (err) { resp.send("error in api") }
-    else { resp.send(result) }
+    if (err) { res.send("error in api") }
+    else { res.send(result) }
   })
 });
+
+
 
 
 
