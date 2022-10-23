c0f6741e91974dfd1ece479d5f5569cf535f6f78
Divya Yash <divyayashsaxena2000@gmail.com>
2022-10-23 07:54:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ res.send("BPH Sports API is up and running")
 app.get("/websites", (req, res) => {
   con.query("select * from collection where 1", (err, result) => {
     if (err) {
-      res.status(404).send({statusId: 404, message: err})
+      res.status(404).send({statusId: 404, message: "Error Occured})
     }
     else { 
-      res.status(200).send({ message: "Data Fetched", sites: result, statusId: 200})
+      res.status(200).send({ message: "Data Fetched.", sites: result, statusId: 200})
     }
   })
 });
